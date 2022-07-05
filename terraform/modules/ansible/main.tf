f3802822e6bd9937ec1e64796559c28178b688c6
briantical <lutbrianivan@gmail.com>
2022-07-05 11:24:53 +0300

    Attempting  terraform trigger with ansible directory checksum

--- terraform/modules/ansible/main.tf
+++ terraform/modules/ansible/main.tf
@@ -13,7 +13,7 @@ resource "local_file" "ansible_inventory" {
 resource "null_resource" "ansible_provisioner" {
   triggers = {
       # https://stackoverflow.com/a/66501021
-      ansible_sha1 = sha1(join("", [for f in fileset("../../../ansible", "**"): filesha1(f)]))
+      ansible_sha1 = sha1(join("", [for f in fileset("../../../ansible", "**"): filesha1("../../../ansible/${f}")]))
   }
 
   provisioner "local-exec" {
